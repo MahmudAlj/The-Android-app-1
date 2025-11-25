c31c942612d0c0832d1cd7feca67f4a79c7b65f3
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2025-11-25 04:26:42 +0300

    login

--- app/src/main/java/com/example/myapplicationennew/LoginActivity.kt
+++ app/src/main/java/com/example/myapplicationennew/LoginActivity.kt
@@ -15,23 +15,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        db = DatabaseHelper(this)
-
         val email = findViewById<EditText>(R.id.loginEmail)
         val password = findViewById<EditText>(R.id.loginPassword)
         val btn = findViewById<Button>(R.id.loginBtn)
 
         btn.setOnClickListener {
-            val emailText = email.text.toString()
-            val passText = password.text.toString()
-
-            if (db.loginUser(emailText, passText)) {
-                Toast.makeText(this, "Giriş başarılı!", Toast.LENGTH_SHORT).show()
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            } else {
-                Toast.makeText(this, "Bilgiler yanlış!", Toast.LENGTH_SHORT).show()
-            }
+            // ❗ Kullanıcı ne yazarsa yazsın kabul et
+            Toast.makeText(this, "Giriş başarılı!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
+
 }
