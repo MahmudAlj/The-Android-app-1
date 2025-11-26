526bbfc14633576ca0ee01f958bbc678f8ce64a9
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2025-11-26 06:08:34 +0300

    ss

--- app/src/main/java/com/example/myapplicationennew/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationennew/MainActivity.kt
@@ -25,15 +25,11 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-// iş eklerken gıder ekleme orda olucak
-// takvımde ıslerenın eklendıgı ısler gozukmesı gerekıyor
-//iş silme uzun tutma
-//hesap makınesı tekrar bakılacak
-// total dogru hesaplanmıyor gıderıde ekleme bır sey olmasın o dırek ıs te bır secenek olsun gıder dıye  ısım gıbı olucak ama totalde eklenıcek - olarak
+// iş silme uzun tutma
 // ıs veya ıs verende ayar dıye bır sey olsun onun otomatık onaylama yada sılme orda olsun
 // işlerde sadece ücret ve iş olsun ustune bastıgında dıger detaylar cıksın
 // uc nokta hep gozuksun
-
+// tekrar eden tuslari ayni kullanmak icin ayri xml de tanimla
 // bunlardan baska gırıs sayfası maıl ıle gırıs maıl ıle kaydolus
 // reklamı aarastırma nasıl google playa yuklıyebılırız
 
